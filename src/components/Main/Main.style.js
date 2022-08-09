d5d61456442fc5f9d1b972817e07ba0b3f29dbb0
viniciospereirarodrigues@gmail.com <viniciospereirarodrigues@gmail.com>
2022-08-09 00:20:39 -0300

    estilos da tabela

--- src/components/Main/Main.style.js
+++ src/components/Main/Main.style.js
@@ -1,5 +1,23 @@
 import styled from "styled-components";
 
 export const Container = styled.main`
-  background: red;
+  background: #929692;
+  margin: 2rem auto 0;
+  max-width: 60rem;
+
+  table {
+    width: 100%;
+  }
+
+  th {
+    border: 1px solid white;
+    padding: 0.5rem;
+    color: white;
+    background: #454745;
+  }
+  td {
+    text-align: center;
+    border: 1px solid white;
+    padding: 0.5rem 0;
+  }
 `;
